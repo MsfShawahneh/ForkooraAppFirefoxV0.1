1914ba7cd2d4f7c1b778f2352c9b677e4a32ab51
Mohammad Shawahneh <msf1996@live.com>
2013-02-17 22:54:35 -0800

    Forkoora.com

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function geteUrlVars(eurl) {
+    var vars = {};
+    var parts = eurl.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+$(document).ready(function(){
+    $("body").hide();
+	$('body').fadeIn(1000);
+
+ 
+});
